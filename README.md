af62dc3ce1705e378892db33bda83e7f82ede35e
DESKTOP-JP1MEGQ\Wang <brzden32@gmail.com>
2021-05-12 19:17:04 -0700

    all delete

--- README.md
+++
@@ -1,6 +0,0 @@
-# Node.js Rest APIs with Express & MySQL for JobSage
-
-### Run
-```
-npm run server
-```
--- app/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  HOST: "35.223.167.126",
-  USER: "Dennis",
-  PASSWORD: "Test123",
-  DB: "rekomdb1"
-};
